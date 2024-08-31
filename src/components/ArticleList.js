03004176ca4a911ccfb6a362da09343ca3b91f9f
khadija <khadijajelle3@gmail.com>
2024-08-31 16:26:58 +0300

    complete lab

---
+++ src/components/ArticleList.js
@@ -0,0 +1,15 @@
+import Article from "./Article";
+function ArticleList({posts}){
+  return(
+<main>
+{posts.map((post)=>(
+  <Article key={post.id} 
+  title={post.title}
+  preview={post.preview} 
+  date={post.date}/>
+))
+}
+</main>
+  );
+}
+export default ArticleList;
